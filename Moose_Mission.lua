006006e799e68aa0ac2f594740479c536c582836
RaynorD <unstoppabledave@gmail.com>
2019-03-20 04:13:25 -0500

    Start from scratch

--- Moose_Mission.lua
+++ Moose_Mission.lua
@@ -1,35 +1,119 @@
--- Parameters
-local SpawnCountCasZones = 20
-local SpawnCountSamZones = 5
+env.info("Starting dynamic load 2")
 
--- Zone Groups
-local ZoneTable_CAS = {}
-local ZoneTable_SAM = {}
+CAS_TaskInfo = {}
 
-for cas_i=1,30,1 do
-  table.insert(ZoneTable_CAS, ZONE:New("Zone CAS "..cas_i))
+function SpawnCASZone(difficulty,mission,attackSet,taskName)
+    local TaskInfo = {}
+    
+    local spawnGroup = SPAWN:New("CAS Target "..difficulty)
+        :InitRandomizeTemplatePrefixes("Spawn CAS "..difficulty)
+        :InitLimit(999,1)
+        :OnSpawnGroup(
+            function(group,difficulty)
+                CommandCenter:MessageToCoalition("New target group located at CAS Target ("..difficulty..").")
+                
+                group:HandleEvent(EVENTS.Dead)
+                function group:OnEventDead(EventData)
+                    env.info("unit killed")
+                    local unitGroup = GROUP:Find(EventData.IniDCSGroup)
+                    local unitName = EventData.IniUnitName
+                    local groupHealth = unitGroup:GetSize() / unitGroup:GetInitialSize()
+                    MESSAGE:New("CAS target ("..unitName..") killed, group health: "..unitGroup:GetSize().."/"..unitGroup:GetInitialSize(),10,"Debug"):ToBlue()
+                end
+            end,
+            difficulty
+        )
+        :SpawnScheduled(60,0)
+
+    TaskInfo.group = spawnGroup
+
+    --TaskInfo.task = TASK_A2G_CAS:New(
+    --    mission,
+    --    attackSet,
+    --    "CAS "..difficulty.." ("..taskName..")",
+    --    SET_UNIT:New():FilterPrefixes("CAS Target "..difficulty):FilterStart(),
+    --    "Engage "..difficulty.." CAS targets (AA Threat: "..taskName..")"
+    --)
+    
+    TaskInfo.jtac = SPAWN:New("JTAC "..difficulty)
+        :InitLimit(1,999)
+        :Spawn()
+    
+    CAS_TaskInfo[difficulty] = TaskInfo
 end
-for sam_i=1,13,1 do
+
+-- CAP
+Spawn_CAP = {}
+ZoneTable_CAP = {
+    ZONE:New("Zone Spawn CAP 1"),
+    ZONE:New("Zone Spawn CAP 2"),
+    ZONE:New("Zone Spawn CAP 3")
+}
+Spawn_CAP_Avail = true
+function SpawnCAP(difficulty)
+    if Spawn_CAP_Avail then
+        --local previousGroup = SET_GROUP:New():FilterPrefixes("CAP Target "..difficulty):FilterStart():GetFirst()
+        
+        CommandCenter:MessageToCoalition("EWR has detected incoming enemy air. ("..difficulty..")")
+        
+        math.random()
+        math.random()
+        math.random()
+        local ZoneCAP = ZoneTable_CAP[math.random(#ZoneTable_CAP)]
+        local SpawnObj = Spawn_CAP[difficulty]
+        local CapGroup = SpawnObj:SpawnInZone(ZoneCAP,true,6000,10000)
+        
+        SCHEDULER:New(nil, function(CapGroup)
+            if CapGroup ~= nil then
+                local alt = CapGroup:GetUnit(1):GetAltitude()
+                local speed = CapGroup:GetSpeedMax()*0.3
+                local coord = CapGroup:GetCoordinate()
+                CapGroup:SetTask(CapGroup:TaskOrbitCircle(alt,speed,coord))
+                Spawn_CAP_Avail = false
+                SCHEDULER:New(nil, ResetCASDelay, {}, 20)
+                
+                --if previousGroup
+                --    if previousGroup:GetName() == CapGroup:GetName() then
+                --        CommandCenter:MessageToCoalition("Group not spawned, previous group still active")
+                --    end
+                --end
+            end
+        end, {CapGroup}, 2)
+        
+        
+        
+    else
+        env.info("CAP Spawn delayed")
+    end
+end
+function ResetCASDelay()
+    Spawn_CAP_Avail = true
+    env.info("CAP Spawn enabled")
+end
+Spawn_CAP["Easy"] = SPAWN:New("CAP Target Easy")
+    :InitRandomizeTemplatePrefixes("Spawn CAP Easy")
+    :InitLimit(2,999)
+    :InitRepeatOnLanding()
+Spawn_CAP["Medium"] = SPAWN:New("CAP Target Medium")
+    :InitRandomizeTemplatePrefixes("Spawn CAP Medium")
+    :InitLimit(2,999)
+    :InitRepeatOnLanding()
+Spawn_CAP["Hard"] = SPAWN:New("CAP Target Hard")
+    :InitRandomizeTemplatePrefixes("Spawn CAP Hard")
+    :InitLimit(2,999)
+    :InitRepeatOnLanding()
+
+-- SAMs
+local SpawnCountSamZones = 5
+local ZoneTable_SAM = {}
+for sam_i=1,5,1 do
   table.insert(ZoneTable_SAM, ZONE:New("Zone SAM "..sam_i))
 end
---
---function SpawnCASTarget()
---  Spawn_Group_CAS_Target:Spawn()
---end
-
-Spawn_CAS_Target = SPAWN:New( "CAS Target" )
-  :InitRandomizeTemplatePrefixes( "Spawn CAS" ) 
-  :InitRandomizeZones(ZoneTable_CAS)
-  :InitLimit(9999, SpawnCountCasZones)
-  
 Spawn_SAM_Target = SPAWN:New( "SAM Target" )
-  :InitRandomizeTemplatePrefixes( "Spawn SAM" ) 
-  :InitRandomizeZones(ZoneTable_SAM)
-  :InitLimit(9999, SpawnCountSamZones)
-  
-for i=1,SpawnCountCasZones,1 do
-  Spawn_CAS_Target:Spawn()
-end
+    :InitRandomizeTemplatePrefixes( "Spawn SAM" )
+    :InitRandomizeZones(ZoneTable_SAM)
+    :InitLimit(9999, SpawnCountSamZones)
+
 for i=1,SpawnCountSamZones,1 do
   Spawn_SAM_Target:Spawn()
 end
@@ -44,106 +128,106 @@ CommandCenter = COMMANDCENTER:New( HQ, "Pegasus" )
 CommandCenter:MessageToCoalition("Operation Kickoff underway. Request tasking using the F10 menu.")
 
 Mission_Watchtower = MISSION
-  :New(CommandCenter, "Watchtower", "Primary", "Defend the valley from hostile invasion.", coalition.side.BLUE)
+    :New(CommandCenter, "Watchtower", "Primary", "Defend the valley from hostile invasion.", coalition.side.BLUE)
+
+Mission_Watchtower:Start()
 
 Mission_Overlord = MISSION
-  :New(CommandCenter, "Overlord", "Primary", "Conduct offensive strikes.", coalition.side.BLUE)
+    :New(CommandCenter, "Overlord", "Primary", "Conduct offensive strikes.", coalition.side.BLUE)
+
+Mission_Overlord:Start()
 
 ---- Setup Menus
-MenuTesting = MENU_COALITION:New( coalition.side.BLUE, "Spawn Menu" )
+MenuSpawn = MENU_COALITION:New( coalition.side.BLUE, "Spawn Menu" )
+MenuDebug = MENU_COALITION:New( coalition.side.BLUE, "Debug Menu" )
 
---MENU_COALITION_COMMAND:New( coalition.side.BLUE, "Spawn CAP", MenuTesting, SpawnCAPTarget )
---MENU_COALITION_COMMAND:New( coalition.side.BLUE, "Destroy CAP", MenuTesting, DebugDestroyCAP )
+MENU_COALITION_COMMAND:New( coalition.side.BLUE, "Spawn CAP (Easy)", MenuSpawn, SpawnCAP, "Easy" )
+MENU_COALITION_COMMAND:New( coalition.side.BLUE, "Spawn CAP (Medium)", MenuSpawn, SpawnCAP, "Medium" )
+MENU_COALITION_COMMAND:New( coalition.side.BLUE, "Spawn CAP (Hard)", MenuSpawn, SpawnCAP, "Hard" )
+--MENU_COALITION_COMMAND:New( coalition.side.BLUE, "Destroy CAP", MenuDebug, DebugDestroyCAP )
 
-MENU_COALITION_COMMAND:New( coalition.side.BLUE, "Destroy one CAS target", MenuTesting,
-  function() 
-    local units = JTACCurrentTargetGroup:GetUnits()
-    local unit = units[math.random(#units)]
-    unit:Destroy(true)
-  end
+MENU_COALITION_COMMAND:New( coalition.side.BLUE, "Destroy one CAS target", MenuDebug,
+    function()
+        local units = SET_UNIT:New():FilterPrefixes("CAS Target Easy"):FilterStart():GetFirst()
+        local unit = units[math.random(#units)]
+        unit:GetCoordinate():Explosion(10)
+    end
+)
+
+MENU_COALITION_COMMAND:New( coalition.side.BLUE, "Destroy VIP target", MenuDebug,
+    function()
+        local unit = SET_UNIT:New():FilterPrefixes("VIP Target"):FilterStart():GetFirst()
+        unit:GetCoordinate():Explosion(10)
+    end
 )
 
 Spawn_CAP_Border = SPAWN:New("CAP Border")
-  :InitRandomizeTemplatePrefixes( "Spawn CAP" )
-  :InitRepeatOnLanding()
-  :InitLimit(2,999)
-  :SpawnScheduled(3600,0)
-  :SpawnScheduleStart()
+    :InitRandomizeTemplatePrefixes( "Spawn CAP" )
+    :InitRepeatOnLanding()
+    :InitLimit(2,999)
+    :SpawnScheduled(600,0)
+    :SpawnScheduleStart()
 
+-- Support
 Texaco_Spawn = SPAWN:New("Texaco")
-  :InitRepeatOnLanding()
-  :InitLimit(1,999)
-  :SpawnScheduled(60,0)
-  :SpawnScheduleStart()
-  
+    :InitRepeatOnLanding()
+    :InitLimit(1,999)
+    :SpawnScheduled(60,0)
+    :SpawnScheduleStart()
+    
 Arco_Spawn = SPAWN:New("Arco")
-  :InitRepeatOnLanding()
-  :InitLimit(1,999)
-  :SpawnScheduled(60,0)
-  :SpawnScheduleStart()
-  
+    :InitRepeatOnLanding()
+    :InitLimit(1,999)
+    :SpawnScheduled(60,0)
+    :SpawnScheduleStart()
+    
 AWACS_Spawn = SPAWN:New("USA AWACS")
-  :InitRepeatOnLanding()
-  :InitLimit(1,999)
-  :SpawnScheduled(60,0)
-  :SpawnScheduleStart()
+    :InitRepeatOnLanding()
+    :InitLimit(1,999)
+    :SpawnScheduled(60,0)
+    :SpawnScheduleStart()
 
-function JTACMoveToNearestGroup()
-  -- FindNearestGroupFromPointVec2 is getting nil data after group respawn
-  local ClosestEnemyGroup = SET_GROUP:New()
-    :FilterPrefixes("CAS Target")
-    :FilterOnce()
-    :FindNearestGroupFromPointVec2(JTACGroup:GetPointVec2())
-  
-  JTACCurrentTargetGroup = ClosestEnemyGroup
-  
-  local pos = ClosestEnemyGroup:GetCoordinate()
-  
-  MESSAGE:New("JTAC orbiting new target group at "..pos:ToStringLLDMS(),10,"JTAC"):ToBlue()
-  
-  JTACGroup:SetTask(JTACGroup:TaskOrbitCircle(2000,150,pos))
-end
-
-function JTACScheduled()
-  MESSAGE:New("Target group ("..#(JTACCurrentTargetGroup:GetUnits())..") health: "..JTACCurrentTargetGroup:GetLife().." / "..JTACCurrentTargetGroup:GetLife0(),10,"JTAC"):ToBlue()
-  if #(JTACCurrentTargetGroup:GetUnits()) <= 2 then
-    JTACCurrentTargetGroup:Destroy(true)
-    MESSAGE:New("Target group is in retreat. JTAC finding new target group.",10,"JTAC"):ToBlue()
-    SCHEDULER:New(JTACGroup, JTACMoveToNearestGroup, {}, 5)
-  end
-end
-
-function onJTACSpawn(group)
-  JTACGroup = group
-  JTACMoveToNearestGroup()
-
-  if JTAC_Designate ~= nil then
-    JTAC_Designate:Remove()
-  end
-  
-  JTAC_Designate = DESIGNATE:New(CommandCenter, JtacDetection, CASAttackSet)
-  JTAC_Designate:SetLaserCodes({1688})
-
-  JtacScheduler = SCHEDULER:New(JTACGroup, JTACScheduled, {}, 5, 5)
-end
-
-JTAC_1_Spawn = SPAWN:New("JTAC 1")
-  :InitRepeatOnLanding()
-  :InitLimit(1,999)
-  :OnSpawnGroup(onJTACSpawn)
-  :SpawnScheduled(60,0)
-  :SpawnScheduleStart()
-
+-- Tasking
 CASAttackSet = SET_GROUP:New()
-  :FilterCoalitions("blue")
-  :FilterStart()
+    :FilterCoalitions("blue")
+    :FilterStart()
 
 JTACSet = SET_GROUP:New()
-  :FilterPrefixes( "JTAC" )
-  :FilterCoalitions("blue")
-  :FilterStart()
+    :FilterPrefixes( "JTAC" )
+    :FilterStart()
 
-JtacDetection = DETECTION_AREAS:New( JTACSet, 20000 )
+env.info("Creating tasks")
 
-CASDispatcher = TASK_A2G_DISPATCHER:New(Mission_Watchtower, CASAttackSet, JtacDetection)
+SpawnCASZone("Easy",Mission_Watchtower,CASAttackSet,"None")
+SpawnCASZone("Medium",Mission_Watchtower,CASAttackSet,"AAA")
+SpawnCASZone("Hard",Mission_Watchtower,CASAttackSet,"AAA/SA-9")
 
+--JtacDetection = DETECTION_AREAS:New( JTACSet, 20000 )
+--
+--CASDispatcher = TASK_A2G_DISPATCHER:New(Mission_Watchtower, CASAttackSet, JtacDetection)
+
+-- VIP Aircraft ===================================================================================
+local vipSpawns = {
+    "VIP Target Nalchik",
+    "VIP Target Mineralnye",
+    "VIP Target Mozdok",
+    "VIP Target Beslan"
+}
+math.random()
+math.random()
+math.random()
+local randomVip = math.random(#vipSpawns)
+MESSAGE:New("Vip selection: "..randomVip.."/"..#vipSpawns,10,"Debug"):ToBlue()
+SPAWN:New(vipSpawns[randomVip])
+    :InitUnControlled(true)
+    :Spawn()
+
+function MonitorVIPStatus()
+    if SET_UNIT:New():FilterPrefixes("VIP Target"):FilterStart():GetFirst():GetLife() < 10 then
+        MESSAGE:New("Vip transport confirmed to be neutralized, well done.",10,"Debug"):ToBlue()
+    else
+        timer.scheduleFunction(checkVIPStatus, {}, timer.getTime() + 60)
+    end
+end
+
+MonitorVIPStatus()
