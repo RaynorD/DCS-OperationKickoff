b01044471f885aa673a5c53575a1d0cb050edcc8
RaynorD <unstoppabledave@gmail.com>
2019-03-20 20:36:29 -0500

    update moose and stennis airboss

--- Moose_Airboss.lua
+++ Moose_Airboss.lua
@@ -4,8 +4,19 @@ local AirbossStennis=AIRBOSS:New("USS Stennis")
 -- Add your custom settings here!
 AirbossStennis:SetTACAN(74, "X", "STN")
 AirbossStennis:SetICLS(5, "STN")
-  AirbossStennis:SetMarshalRadio(305)
-  AirbossStennis:SetLSORadio(125)
+AirbossStennis:SetMarshalRadio(305)
+AirbossStennis:SetLSORadio(125)
+AirbossStennis:SetSoundfilesFolder("Airboss Soundfiles/")
+AirbossStennis:SetMenuSingleCarrier()
+
+-- Load all saved player grades from your "Saved Games\DCS" folder (if lfs was desanitized).
+AirbossStennis:Load()
+
+-- Automatically save player results to your "Saved Games\DCS" folder each time a player get a final grade from the LSO.
+AirbossStennis:SetAutoSave()
+
+-- Enable trap sheet.
+AirbossStennis:SetTrapSheet()
    
 -- Delete auto recovery window.
 function AirbossStennis:OnAfterStart(From,Event,To)
@@ -33,12 +44,22 @@ local function StopRecovery()
   AirbossStennis:RecoveryStop()
 end
 
-local menucarriercontrol=MENU_COALITION:New(AirbossStennis:GetCoalition(), "Carrier Control")
+local menucarriercontrol=MENU_COALITION:New(AirbossStennis:GetCoalition(), "Airboss")
 MENU_COALITION_COMMAND:New(AirbossStennis:GetCoalition(), "Start CASE I",   menucarriercontrol, StartRecovery, 1)
 MENU_COALITION_COMMAND:New(AirbossStennis:GetCoalition(), "Start CASE II",  menucarriercontrol, StartRecovery, 2)
 MENU_COALITION_COMMAND:New(AirbossStennis:GetCoalition(), "Start CASE III", menucarriercontrol, StartRecovery, 3)
 MENU_COALITION_COMMAND:New(AirbossStennis:GetCoalition(), "Stop Recovery",  menucarriercontrol, StopRecovery)
 
-
-RescueheloStennis = RESCUEHELO:New(UNIT:FindByName("USS Stennis"), "Stennis Rescue")
+RescueheloStennis = RESCUEHELO:New("USS Stennis", "Stennis Rescue")
 RescueheloStennis:Start()
+
+-- S-3B at USS Stennis spawning on deck.
+local tankerStennis=RECOVERYTANKER:New("USS Stennis", "Stennis Tanker")
+tankerStennis:SetTACAN(50, "SHE")
+tankerStennis:SetRadio(230)
+-- Start recovery tanker.
+tankerStennis:Start()
+
+-- Set carrier strike groups to patrol waypoints indefinitely. Once the last waypoint is reached, group will go back to first waypoint.
+local StennisGroup=GROUP:FindByName("USS Stennis")
+StennisGroup:PatrolRoute()
