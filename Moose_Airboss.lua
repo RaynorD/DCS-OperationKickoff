6f410dd5c498559f28a5bccfde20b273ec73d410
RaynorD <unstoppabledave@gmail.com>
2019-04-02 23:40:40 -0500

    - Add briefing images - Add kneeboard info image - Add waypoint names - Set Arco speed to 250 IAS - Add command unit to Hawks - Add search radar to border SA-2 template - Remove Patriots - Rename tankers to show info on map - Remove BAI bugging out message - Remove Airboss recovery window menu

--- Moose_Airboss.lua
+++ Moose_Airboss.lua
@@ -1,6 +1,7 @@
 -- Create AIRBOSS object.
 local AirbossStennis=AIRBOSS:New("USS Stennis")
 
+
 -- Add your custom settings here!
 AirbossStennis:SetTACAN(74, "X", "STN")
 AirbossStennis:SetICLS(5, "STN")
@@ -13,46 +14,37 @@ AirbossStennis:SetRadioRelayLSO("Stennis LSO Relay")
 AirbossStennis:SetRadioRelayMarshal("Stennis Marshal Relay")
 --AirbossStennis:SetMenuSingleCarrier()
 
--- Load all saved player grades from your "Saved Games\DCS" folder (if lfs was desanitized).
-AirbossStennis:Load()
-
--- Automatically save player results to your "Saved Games\DCS" folder each time a player get a final grade from the LSO.
-AirbossStennis:SetAutoSave()
-
--- Enable trap sheet.
-AirbossStennis:SetTrapSheet()
-   
--- Delete auto recovery window.
-function AirbossStennis:OnAfterStart(From,Event,To)
-  self:DeleteAllRecoveryWindows()
-end
-
--- Start airboss script.
-AirbossStennis:Start()
-
--- Start recovery function.
-local function StartRecovery(case)
-
-  -- Recovery staring in 5 min for 30 min.
-  local t0=timer.getAbsTime()+5*60
-  local t9=t0+30*60
-  local C0=UTILS.SecondsToClock(t0)
-  local C9=UTILS.SecondsToClock(t9)
-
-  -- Carrier will turn into the wind. Wind on deck 25 knots. U-turn on.
-  AirbossStennis:AddRecoveryWindow(C0, C9,case, 30, true, 25, true)
-end
-
--- Stop recovery function.
-local function StopRecovery()
-  AirbossStennis:RecoveryStop()
-end
-
-local menucarriercontrol=MENU_COALITION:New(AirbossStennis:GetCoalition(), "Airboss Recovery")
-MENU_COALITION_COMMAND:New(AirbossStennis:GetCoalition(), "Start CASE I",   menucarriercontrol, StartRecovery, 1)
-MENU_COALITION_COMMAND:New(AirbossStennis:GetCoalition(), "Start CASE II",  menucarriercontrol, StartRecovery, 2)
-MENU_COALITION_COMMAND:New(AirbossStennis:GetCoalition(), "Start CASE III", menucarriercontrol, StartRecovery, 3)
-MENU_COALITION_COMMAND:New(AirbossStennis:GetCoalition(), "Stop Recovery",  menucarriercontrol, StopRecovery)
+---- Delete auto recovery window.
+--function AirbossStennis:OnAfterStart(From,Event,To)
+--  self:DeleteAllRecoveryWindows()
+--end
+--
+---- Start airboss script.
+--AirbossStennis:Start()
+--
+---- Start recovery function.
+--local function StartRecovery(case)
+--
+--  -- Recovery staring in 5 min for 30 min.
+--  local t0=timer.getAbsTime()+5*60
+--  local t9=t0+30*60
+--  local C0=UTILS.SecondsToClock(t0)
+--  local C9=UTILS.SecondsToClock(t9)
+--
+--  -- Carrier will turn into the wind. Wind on deck 25 knots. U-turn on.
+--  AirbossStennis:AddRecoveryWindow(C0, C9,case, 30, true, 25, true)
+--end
+--
+---- Stop recovery function.
+--local function StopRecovery()
+--  AirbossStennis:RecoveryStop()
+--end
+--
+--local menucarriercontrol=MENU_COALITION:New(AirbossStennis:GetCoalition(), "Airboss Recovery")
+--MENU_COALITION_COMMAND:New(AirbossStennis:GetCoalition(), "Start CASE I",   menucarriercontrol, StartRecovery, 1)
+--MENU_COALITION_COMMAND:New(AirbossStennis:GetCoalition(), "Start CASE II",  menucarriercontrol, StartRecovery, 2)
+--MENU_COALITION_COMMAND:New(AirbossStennis:GetCoalition(), "Start CASE III", menucarriercontrol, StartRecovery, 3)
+--MENU_COALITION_COMMAND:New(AirbossStennis:GetCoalition(), "Stop Recovery",  menucarriercontrol, StopRecovery)
 
 RescueheloStennis = RESCUEHELO:New("USS Stennis", "Stennis Rescue")
 RescueheloStennis:Start()
